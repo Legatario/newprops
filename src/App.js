b3726d5dcbce94e5d9733640299f636b823ce17d
Legatario <l.phillipe.d@gmail.com>
2023-05-26 17:13:28 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React from 'react';
 import './App.css';
+import Slick from './componentes/Slick'
+
 
 function App() {
   return (
     <div className="App">
-      oi
+      <Slick />
     </div>
   );
 }
